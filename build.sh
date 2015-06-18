83f0bb023794e491274aeab58f14c602fa13f1c1
Thomas ZILLIOX <thomas@zilliox.me>
2015-06-18 14:49:22 +0200

    Separate the atomic classes & add a bunch of new ones

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
+sass src/chewing-grid.scss build/chewing-grid.css --style expanded
+sass src/chewing-grid.scss build/chewing-grid.min.css --style compressed
+
 sass src/chewing-grid-atomic.scss build/chewing-grid-atomic.css --style expanded
 sass src/chewing-grid-atomic.scss build/chewing-grid-atomic.min.css --style compressed
