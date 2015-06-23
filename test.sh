3e7021cfb89e47e3f428dcc01c47949b4b730370
Thomas ZILLIOX <thomas@zilliox.me>
2015-06-23 17:37:36 +0200

    Reorganize sass testing script

--- test.sh
+++ test.sh
@@ -20,36 +20,46 @@ fi;
 
 for SCSS_FILE in $(echo ${SCSS_FILE_LIST});
 do
+
+  # Is source file correct
   if [ ! -f ${SCSS_FILE} -o ${SCSS_FILE##*.} != "scss" ];
   then
-    echo "${SCSS_FILE}: NOT FOUND"
-  else
-    CSS_FILE=$( echo ${SCSS_FILE%.*}.css);
-    if [ ! -f ${CSS_FILE} ];
-    then
-      echo "No existing result test for '${SCSS_FILE}'."
-      confirm "generate a new one?"
-      if [ $ANSWER == true ];
-      then
-        sass ${SCSS_FILE} ${CSS_FILE}
-      fi;
-    fi;
-    DIFF=$( sass ${SCSS_FILE} | diff ${CSS_FILE} - | wc -l);
-    if [ $DIFF -gt 0 ];
-    then
-      echo "${SCSS_FILE}: ERROR"
-      confirm "see the difference?"
-      if [ $ANSWER == true ];
-      then
-        sass ${SCSS_FILE} | diff --side-by-side ${CSS_FILE} -;
-        confirm "override the current result?"
-        if [ $ANSWER == true ];
-        then
-          sass ${SCSS_FILE} ${CSS_FILE}
-        fi;
-      fi;
-    else
-      echo "${SCSS_FILE}: OK";
-    fi;
+    echo "${SCSS_FILE}: NOT FOUND";
+    continue;
   fi;
+  
+  # If expected result exists
+  CSS_FILE=$( echo ${SCSS_FILE%.*}.css);
+  if [ ! -f ${CSS_FILE} ];
+  then
+    echo "No existing result test for '${SCSS_FILE}'."
+    confirm "generate a new one?"
+    if [ $ANSWER == true ];
+    then
+      sass ${SCSS_FILE} ${CSS_FILE}
+    fi;
+    continue;
+  fi;
+  
+  # Compare compiled & expected
+  DIFF=$( sass ${SCSS_FILE} | diff ${CSS_FILE} - | wc -l);
+  if [ $DIFF -eq 0 ];
+  then
+    echo "${SCSS_FILE}: OK";
+    continue;
+  fi;
+  echo "${SCSS_FILE}: ERROR"
+  
+  confirm "see the difference?"
+  if [ $ANSWER == true ];
+  then
+    sass ${SCSS_FILE} | diff --side-by-side - ${CSS_FILE};
+  fi;
+  
+  confirm "override the current result?"
+  if [ $ANSWER == true ];
+  then
+    sass ${SCSS_FILE} ${CSS_FILE}
+  fi;
+  
 done;
